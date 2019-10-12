f1a8f0eea29859f79b927cf3137889eff0a218ca
supercoww <ambar.ytl@gmail.com>
2019-10-12 14:24:44 +0530

    add paddle motion update and render code

--- js/paddle.js
+++ js/paddle.js
@@ -1,12 +1,42 @@
+const PADDLE_SPEED = 200;
+
 export class Paddle {
-	constructor(x, y, width, height) {
+	constructor(x, y, keys, canvasContext) {
 		this.x = x;
 		this.y = y;
-		this.width = width;
-		this.height = height;
+		this.width = 5;
+		this.height = 20;
+		this.vy = 0;
+
+		this.canvasContext = canvasContext;
+		this.canvas = canvasContext.canvas;
+
+		this.listenKeyboard(keys);
 	}
 
-	update(dt) {}
+	listenKeyboard(keys) {
+		document.addEventListener('keydown', key => {
+			if (key.keyCode === keys.up) {
+				this.vy = -PADDLE_SPEED;
+			} else if (key.keyCode === keys.down) {
+				this.vy = PADDLE_SPEED;
+			} else {
+				this.vy = 0;
+			}
+		});
 
-	render() {}
+		document.addEventListener('keyup', () => (this.vy = 0));
+	}
+
+	update(dt) {
+		if (this.vy < 0) {
+			this.y = Math.max(0, this.y + this.vy * dt);
+		} else {
+			this.y = Math.min(this.canvas.height - this.height, this.y + this.vy * dt);
+		}
+	}
+
+	render() {
+		this.canvasContext.fillRect(this.x, this.y, this.width, this.height);
+	}
 }
