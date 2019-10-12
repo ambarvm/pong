beafef113b9b7d20914225680148005275fe7606
divyanshk443 <55971596+divyanshk443@users.noreply.github.com>
2019-10-12 12:19:47 +0530

    display FPS

--- js/game.js
+++ js/game.js
@@ -8,14 +8,22 @@ canvas.width = 432;
 canvas.height = 243;
 
 context.font = '15px retro';
-context.fillStyle = 'white';
-context.textAlign = 'center';
+
+let fpsCounter = 0;
+
+function displayFPS() {
+	context.fillStyle = 'green';
+	context.textAlign = 'left';
+
+	context.fillText(`FPS:${Game.fps}`, 2, 15);
+}
 
 const Game = {
 	lastFrameTime: null,
+	fps: null,
 	init: function(time) {
 		if (this.lastFrameTime) {
-			this.update(time - this.lastFrameTime);
+			this.update((time - this.lastFrameTime) / 1000);
 			this.render();
 		}
 		this.lastFrameTime = time;
@@ -26,10 +34,18 @@ const Game = {
 	},
 	update: function(dt) {
 		// Update game state here
+		if (fpsCounter == 20) {
+			Game.fps = Math.floor(1 / dt);
+			fpsCounter = 0;
+		}
+		fpsCounter++;
 	},
 	render: function() {
 		// clear the canvas for redraw
 		context.clearRect(0, 0, canvas.width, canvas.height);
+
+		context.fillStyle = 'white';
+		context.textAlign = 'center';
 
 		// All rendering stuff
 		context.fillText('Hello Pong!', canvas.width / 2, 30);
@@ -37,6 +53,8 @@ const Game = {
 
 		context.fillRect(10, 30, 5, 20);
 		context.fillRect(canvas.width - 15, canvas.height - 30, 5, 20);
+
+		displayFPS();
 	}
 };
 
